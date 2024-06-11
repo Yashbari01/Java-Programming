d59633f798b76540fb2445e4aa53e96cffeb098f
Yashbari01 <24yashbari@gmail.com>
2024-06-11 20:57:39 +0530

    New Update in Java add DataTypes

--- src/Datatypes.java
+++ src/Datatypes.java
@@ -2,9 +2,12 @@ public class Datatypes {
     public static void main(String[] args){
 //        println and print different these two that println will display the message on a new line while the print statement will print the message on the same line.
 
+//        There are 8 Primitive data types in Java – Boolean, char, byte, int, short, long, float, and double.
+
 //        int DataType value range lies between -2,147,483,648 to 2,147,483,647.
         int a=10;
-        System.out.println(a);
+        int b=20;
+        System.out.println(a+b);
 
 //        Char DataType value range lies between '\u0000'(or 0) to '\uffff' (or 65,535 inclusive).
         char Data ='A';
@@ -19,15 +22,23 @@ public class Datatypes {
         System.out.println(s1);
 
 //        long DataType value range lies between -9223372036854775808 to 9223372036854775807 (32-bit).
-        long L1= 922337202;
+        long L1= 9223372036854775807L;
         System.out.println(L1);
+
+//        Boolean DataType is only two possible values true/false.
+        boolean bool1= a == b;
+        System.out.println(bool1);
 
 //        float DataType is a single-precision 32-bit IEEE 754 floating point. Its value range is unlimited.
         float f1= 10.1f;
         System.out.println(f1);
 
 //        Double DataType is a double-precision 64-bit IEEE 754 floating point. Its value range is unlimited.
-        double D1= 987887670;
+        double D1= 98.7887670;
         System.out.println(D1);
+
+//        There are 3 Non-Primitive data types in Java – strings, arrays, and classes.
+        String string1 = "Hello I am Yash";
+        System.out.println(string1);
     }
 }
