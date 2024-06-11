a9d216469ea404f3ba9f71146f9aacdfad487978
Yashbari01 <24yashbari@gmail.com>
2024-06-11 20:32:28 +0530

    New Update in Java add DataTypes

--- src/Datatypes.java
+++ src/Datatypes.java
@@ -1,5 +1,25 @@
 public class Datatypes {
     public static void main(String[] args){
-        System.out.println("hello Data Types");
+//        println and print different these two that println will display the message on a new line while the print statement will print the message on the same line.
+
+//        int DataType value range lies between -2,147,483,648 to 2,147,483,647.
+        int a=10;
+        System.out.println(a);
+
+//        Char DataType value range lies between '\u0000'(or 0) to '\uffff' (or 65,535 inclusive).
+        char Data ='A';
+        System.out.println(Data);
+
+//        Byte DataType value range lies between -128 to 127.
+        byte b1=1;
+        System.out.println(b1);
+
+//        short DataType value range lies between -32768 to 32767.
+        short s1=32767;
+        System.out.println(s1);
+
+//        float DataType is a single-precision 32-bit IEEE 754 floating point. Its value range is unlimited.
+        float f1= 10.1f;
+        System.out.println(f1);
     }
 }
